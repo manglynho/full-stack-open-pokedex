12dc64cdd22296850db71c59ac3e292c213c8d5a
Reiner <manglynho@gmail.com>
2022-07-12 15:11:41 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,6 +63,7 @@ module.exports = {
     ],
     "react/jsx-one-expression-per-line": [
       "error", { "allow": "single-child" }
-    ]
+    ],
+    "@testing-library/no-unnecessary-act" : "off"
   }
 }
